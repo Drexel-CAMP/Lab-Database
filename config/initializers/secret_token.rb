575016025c809b0bb3bb79b9fcbccd038c6c6664
Eric LaBouff <eml73@drexel.edu>
2014-02-09 17:24:41 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Ehrlichdatabase::Application.config.secret_key_base = '1a14a730afe690eb94b3e1fc9b5f6243767bb525a54da4cbe3aeb85cb5ba2e269c6839f0e7cb1a6adaf1463fb4d185c44c2ec30b80dcb01184a849a48b8988cb'
